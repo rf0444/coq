286aaa643ffd8aba89ce46825a8e553bb763d36b
rf <rf0444@gmail.com>
2010-09-28 16:03:27 +0900

    Coq練習 整理、追加

--- prop.v
+++ prop.v
@@ -1,3 +1,34 @@
+Theorem and_1 : forall P Q:Prop, P -> Q -> P /\ Q.
+Proof.
+  intros; split; assumption.
+Qed.
+
+Theorem and_2 : forall P Q:Prop, P /\ Q -> P -> Q.
+  intros P Q H _; elim H.
+  intros _ q; assumption.
+Qed.
+
+Theorem nn_1 : forall P:Prop, P -> ~~P.
+Proof.
+  intros P p np.
+  contradiction.
+Qed.
+
+Theorem existn_nforall_1 :
+  forall (A:Type)(P:A -> Prop),
+  (exists a:A, ~ P a) -> (~ forall a:A, P a).
+Proof.
+  intros A P H1 H2; elim H1; auto.
+Qed.
+
+Theorem existn_nforall_2 :
+  forall (A:Type)(P:A -> Prop),
+  (forall a:A, P a) -> (exists a:A, ~ P a) -> False.
+Proof.
+  intros A P H1 H2; elim H2; auto.
+Qed.
+
+
 Lemma exists_forall : forall (A:Type)(P: A -> A -> Prop),
   (exists a:A, forall b:A, P a b) -> (forall b:A, exists a:A, P a b).
 Proof.
@@ -8,36 +39,6 @@ Proof.
   apply H2.
 Qed.
 
-(*
-Lemma forall_exists_1 : forall (A:Type)(P: A -> Prop), (forall a:A, P a) -> (exists a:A, P a).
-Proof.
-  intros A P H1.
-  intro H2.
-
-*)
-(*
-Lemma forall_exists_not : forall (A:Type), exists P: A -> A -> Prop,
-  (forall b:A, exists a:A, P a b) -> ~ (exists a:A, forall b:A, P a b).
-Proof.
-  intro A.
-  exists (fun a b => a = b).
-  intros _.
-  intro H1.
-  elim H1.
-  intros x H2.
-  
-Qed.
-*)
-(*
-Lemma forall_exists_not2 : forall (A:Type), ~ forall (P: A -> A -> Prop),
-  (forall b:A, exists a:A, P a b) -> (exists a:A, forall b:A, P a b).
-Proof.
-  intro A.
-  intro H0.
-  
-  
-Qed.
-*)
 
 Theorem 矛盾 : forall (矛 盾:Type)(突き通す:矛 -> 盾 -> Prop),
   (exists 最強の矛:矛, forall あらゆる盾:盾, 突き通す 最強の矛 あらゆる盾) ->
@@ -53,5 +54,4 @@ Proof.
   apply (最強の盾はあらゆる矛を防ぐ 最強の矛).
   apply (最強の矛はあらゆる盾を貫く 最強の盾).
 Qed.
-
 
